570203ef53545873826290ce1fadc6c09a53172e
J Angel Gonzalez <a01552274@itesm.mx>
2021-05-30 18:59:51 -0500

    Agregando funcion amigo

--- main.cpp
+++ main.cpp
@@ -2,12 +2,30 @@
 
 using namespace std;
 
+class Persona{
+
+    private:
+        int edad;
+        int numFavorito;
+
+    public:
+        Persona(): edad{0}, numFavorito{0} {}
+    
+    friend void friendFunction(Persona &alguien);
+};
+
+
+void friendFunction(Persona &alguien){
+    alguien.edad = 99;
+    cout << alguien.edad << endl;
+
+}
+
 
 int main(){
 
-
-
-
+    Persona bob;
+    friendFunction(bob);
 
     cout << "It works!!!" << endl;
 
