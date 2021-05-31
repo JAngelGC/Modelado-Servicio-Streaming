935a3d0ce04b2e61df75f11541bbf32a9552fa7d
J Angel Gonzalez <a01552274@itesm.mx>
2021-05-30 20:33:46 -0500

    Preparando main

--- main.cpp
+++ main.cpp
@@ -2,38 +2,15 @@
 
 using namespace std;
 
-class Persona{
-
-    private:
-        int edad;
-        int numFavorito;
-
-    public:
-        Persona(): edad{0}, numFavorito{0} {}
-    
-    friend void friendFunction(Persona &alguien);
-};
-
-class Archivo{
-
-    private:
-        int address;
-    public:
-        int getAdress(){return address;}
-
-};
-
-void friendFunction(Persona &alguien){
-    alguien.edad = 99;
-    cout << alguien.edad << endl;
-
-}
-
 
 int main(){
 
-    Persona bob;
-    friendFunction(bob);
+ 
+
+
+
+
+ 
 
     cout << "It works!!!" << endl;
 
